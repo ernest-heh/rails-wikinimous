e19d91fdbc1706bdf206ea1c203462185575fb10
Ernest Ho <ernest.heh@gmail.com>
2023-03-07 16:59:40 +0800

    refactored controller methods using before_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def new
     @article = Article.new
@@ -13,25 +13,18 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.create!(article_params)
-
     redirect_to article_path(@article)
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
     @article.update(article_params)
-
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
-
     redirect_to articles_path, status: :see_other
   end
 
@@ -39,5 +32,9 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
